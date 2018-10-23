dc197d719a66dd3644f7e497c686521bf8a8c862
Jason Lee <jawc@hotmail.com>
2018-10-23 22:11:56 +0800

    add queue subscrible

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -9,38 +9,22 @@ import (
 
 func main() {
 	//stan.Connect(clusterID, clientID, ops ...Option)
-	ns, _ := stan.Connect("test-cluster", "consumer1", stan.NatsURL("nats://localhost:4222"))
-	defer ns.Close()
+	natsConn, _ := stan.Connect("test-cluster", "consumer1", stan.NatsURL("nats://10.200.252.105:4222"))
+	defer natsConn.Close()
 
-	// Simple Async Subscriber
-	sub, _ := ns.Subscribe("foo", func(m *stan.Msg) {
-		m.Ack()
-		log.Printf("Received a message: %s and reply: %S\n ", string(m.Data), m.Reply)
-		//ns.Publish(m.Reply, []byte("reply.."))
-	}, stan.SetManualAckMode(), stan.DurableName("i-will-remember"))
-	log.Printf("subscribing to subject 'foo' \n")
+	sub := QueueSubscribe(natsConn)
+	log.Printf("subscribing to subject 'delivery' \n")
 
-	// nc := ns.NatsConn()
-	// i := 3
-	// nc.Subscribe("foo", func(msg *nats.Msg) {
-	// 	i++
-	// 	log.Printf("reply msg")
-	// 	nc.Publish(msg.Reply, []byte("replya"))
-	// })
-	// nc.Flush()
-
-	// msg, err := nc.Request("foo", []byte("hello jason"), 30*time.Second)
-	// if err != nil {
-	// 	if nc.LastError() != nil {
-	// 		log.Fatalf("Error in Request1: %v\n", nc.LastError())
-	// 	}
-	// 	log.Fatalf("Error in Request2: %v\n", err)
-	// }
-
-	// log.Printf("Received [%v] : '%s'\n", msg.Subject, string(msg.Data))
-
-	time.Sleep(400 * time.Second)
+	time.Sleep(500 * time.Second)
 	// Unsubscribe
 	sub.Unsubscribe()
 
+}
+
+func QueueSubscribe(natsConn stan.Conn) stan.Subscription {
+	sub, _ := natsConn.QueueSubscribe("delivery", "worker1", func(m *stan.Msg) {
+		m.Ack()
+		log.Printf("Received a message: %s\n ", string(m.Data))
+	}, stan.SetManualAckMode(), stan.DurableName("i-will-remember"))
+	return sub
 }
--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -7,9 +7,9 @@ import (
 func main() {
 
 	//stan.Connect(clusterID, clientID, ops ...Option)
-	ns, _ := stan.Connect("test-cluster", "publisher1", stan.NatsURL("nats://localhost:4222"))
+	ns, _ := stan.Connect("test-cluster", "publisher1", stan.NatsURL("nats://10.200.252.105:4222"))
 	defer ns.Close()
 
-	ns.Publish("foo", []byte("Hello consumer4"))
+	ns.Publish("delivery", []byte("Hello consumer4"))
 
 }
